8a914b6f4d4ccdff1912be1bae9abb4001c02753
csunclechen <csunclechen@gmail.com>
2019-03-05 00:24:31 +0800

    Update Extender.v

--- src_files/Extender.v
+++ src_files/Extender.v
@@ -1,26 +1,13 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/03/04 20:39:12
-// Design Name: 
-// Module Name: Extender
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module Extender(
-
-    );
+module Extender 
+#(  parameter BIT_WIDTH_IN=16,
+    parameter BIT_WIDTH_OUT = 32,
+    parameter TYPE=0
+ )
+(
+    input [BIT_WIDTH_IN-1:0] ext_data_in,
+    output [BIT_WIDTH_OUT-1:0] ext_data_out
+); 
+    assign ext_data_out = TYPE?$signed(ext_data_in):$unsigned(ext_data_in);
 endmodule
