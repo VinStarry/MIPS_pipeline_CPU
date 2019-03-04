bf152ee9b8c2dedd1c218a79dbab933aa9486fa1
XJDKC <xjdkcsq3@gmail.com>
2019-03-04 20:46:00 +0800

    Project Structure

---
+++ singleCircularCPU/singleCircularCPU.srcs/sources_1/new/ROM.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/03/04 20:39:12
+// Design Name: 
+// Module Name: ROM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ROM(
+
+    );
+endmodule
