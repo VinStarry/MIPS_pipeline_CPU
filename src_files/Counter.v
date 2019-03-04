a8685801df76287d5f58408fb639e7725930269d
yoyomimi <u201617123@hust.edu.cn>
2019-03-04 23:53:36 +0800

    commit Counter.v

--- src_files/Counter.v
+++ src_files/Counter.v
@@ -20,7 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Counter(
+module Counter(clk, rst, counter_enable, counter_data_out);
+	parameter DATA_BITS = 32;
+	input clk, rst, counter_enable;
+	reg[DATA_BITS-1:0] counter_reg;
+	output[DATA_BITS-1:0] counter_data_out;
 
-    );
+	initial 
+		begin
+			counter_reg <= 0;
+		end
+
+	always @(posedge clk) 
+		begin
+			if(rst)
+				counter_reg <= 0;
+			else if(counter_enable)
+				counter_reg <= counter_reg + 1;
+            	counter_data_out <= counter_reg;
+		end
 endmodule
+
+
