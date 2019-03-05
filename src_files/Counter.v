7ab3e3229bc6ee3e8ad51e305f4d486448d870f4
XJDKC <xjdkcsq3@gmail.com>
2019-03-05 08:54:42 +0800

    Modify Counter.v

--- src_files/Counter.v
+++ src_files/Counter.v
@@ -23,21 +23,15 @@
 module Counter(clk, rst, counter_enable, counter_data_out);
 	parameter DATA_BITS = 32;
 	input clk, rst, counter_enable;
-	reg[DATA_BITS-1:0] counter_reg;
-	output[DATA_BITS-1:0] counter_data_out;
+	output reg[DATA_BITS-1:0] counter_data_out=0;
 
-	initial 
-		begin
-			counter_reg <= 0;
-		end
 
 	always @(posedge clk) 
 		begin
 			if(rst)
-				counter_reg <= 0;
+				counter_data_out <= 0;
 			else if(counter_enable)
-				counter_reg <= counter_reg + 1;
-            	counter_data_out <= counter_reg;
+				counter_data_out <= counter_data_out + 1;
 		end
 endmodule
 
