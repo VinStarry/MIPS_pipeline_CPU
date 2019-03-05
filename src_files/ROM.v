bfb4b93e6ae8fcabc82c7620653664d96b1dfecb
XJDKC <xjdkcsq3@gmail.com>
2019-03-05 17:40:00 +0800

    Update ROM.v,add code.hex

--- src_files/ROM.v
+++ src_files/ROM.v
@@ -21,8 +21,21 @@
 
 
 module ROM(rom_addr,rom_data_out);
-    input [9:0]rom_addr;
-    output [31:0]rom_data_out;
+    input [9:0] rom_addr;			    //指令地址
+    output reg [31:0]rom_data_out;	    //机器指令值
     
+    reg [31:0]data[1023: 0];            //寄存器值
+
+    initial
+    begin
+        rom_data_out=0;
+        $readmemh("code.hex",data,0,1<<9-1);
+    end
+
+
+    always @(rom_addr)
+    begin
+        rom_data_out <= data[rom_addr];
+    end
     
 endmodule
