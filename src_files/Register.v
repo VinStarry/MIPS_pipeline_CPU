901f985bec5431a184c69fa9f5be5e82ed564d73
georgezhang <georgezhang0608@gmail.com>
2019-03-05 10:16:04 +0800

    already simulated

--- src_files/Register.v
+++ src_files/Register.v
@@ -1,26 +1,32 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/03/04 20:39:12
-// Design Name: 
-// Module Name: Register
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module Register #(parameter DATA_BITS = 32)(clk, rst, reg_enable, reg_data_in, reg_data_out);
+  input clk;                    				      //上升沿有效
+  input rst;                    				      //为1时，清空为0
+  input reg_enable;  							            //为1时，时钟有效，将reg_data_in存入寄存器中
+  input [DATA_BITS - 1: 0] reg_data_in;			  //写入数据
+  output reg [DATA_BITS - 1: 0] reg_data_out;	//读出数据
+  reg [DATA_BITS - 1: 0] reg_data;				    //寄存器值
 
-module Register(
+  initial
+  begin
+    reg_data <= 0;
+  end
 
-    );
+  always @(posedge clk)
+  begin
+  	if (rst == 1)
+  	begin
+  		reg_data <= 0;
+  	end
+  	else if (reg_enable == 1)
+  	begin
+  	  reg_data <= reg_data_in;
+  	end
+  end
+
+  always @(*)
+  begin
+  	reg_data_out <= reg_data;
+  end
 endmodule
