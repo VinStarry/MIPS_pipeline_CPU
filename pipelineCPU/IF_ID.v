1134085f1fa515dc6c93f8eccd19095366c6b5fa
yoyomimi <u201617123@hust.edu.cn>
2019-03-10 22:38:20 +0800

    add IF_ID.v & ID/EX.v

--- pipelineCPU/IF_ID.v
+++ pipelineCPU/IF_ID.v
@@ -34,45 +34,45 @@
 
 
 module IF_ID(
-    input clk,
-    input Enable,
-    input rst,
-    input Effective_in,
-    input [31:0]PC_in,
-    input [31:0]IR_in,
-    output reg [31:0]PC_out,
-    output reg [31:0]IR_out,
-    output reg Effective_out
-    // input pre_jmp_in,
-    // input hit_in,
-    // output reg pre_jmp_out,
-    // output reg hit_out
-    );
-    
-    initial begin
-        PC_out <= 0;
-        IR_out <= 0;
-        Effective_out <= 0;
-        // pre_jmp_out <= 0;
-        // hit_out <= 0;
-        end
-    
-    
-    always@ (posedge clk) begin
-        if (!Enable && !rst) begin
-            PC_out <= PC_in;
-            IR_out <= IR_in;
-            Effective_out <= Effective_in;
-            // pre_jmp_out <= pre_jmp_in;
-            // hit_out <= hit_in;
-            end
-        else if (!Enable && rst) begin
-            PC_out <= 0;
-            IR_out <= 0;
-            Effective_out <= 0;
-            // pre_jmp_out <= 0;
-            // hit_out <= 0;
-            end
-    end
-       
+	input clk,
+	input Enable,
+	input rst,
+	input Effective_in,
+	input [31:0]PC_in,
+	input [31:0]IR_in,
+	output reg [31:0]PC_out,
+	output reg [31:0]IR_out,
+	output reg Effective_out
+	// input pre_jmp_in,
+	// input hit_in,
+	// output reg pre_jmp_out,
+	// output reg hit_out
+	);
+	
+	initial begin
+		PC_out <= 0;
+		IR_out <= 0;
+		Effective_out <= 0;
+		// pre_jmp_out <= 0;
+		// hit_out <= 0;
+		end
+	
+	
+	always@ (posedge clk) begin
+		if (!Enable && !rst) begin
+			PC_out <= PC_in;
+			IR_out <= IR_in;
+			Effective_out <= Effective_in;
+			// pre_jmp_out <= pre_jmp_in;
+			// hit_out <= hit_in;
+			end
+		else if (!Enable && rst) begin
+			PC_out <= 0;
+			IR_out <= 0;
+			Effective_out <= 0;
+			// pre_jmp_out <= 0;
+			// hit_out <= 0;
+			end
+	end
+	   
 endmodule
